bda3f28b0e39bca00eb8437c88c93e89584a0cdb
Ruanh <79330021+joseruanh@users.noreply.github.com>
2022-11-14 10:57:18 -0300

    adicionando recursos

---
+++ p/planilha.js
@@ -0,0 +1,27 @@
+function verPontos(){
+    let ruanh = 5;
+    let jean = 4;
+    let pickles = 3;
+    let relampago = 3;
+    let kennyd = 3;
+    let tony = 2;
+    let brener = 1;
+    let ademiro = 1;
+
+    let pontos = window.document.getElementById('pontos');
+    pontos.innerHTML = `
+    
+    <p style="font-weight: bolder;">Total de pontos: <br></p>
+                <p> Ruanh: ${ruanh} <br>
+                    Jean: ${jean} <br>
+                    Pickles: ${pickles} <br>
+                    Relâmpago: ${relampago} <br>
+                    Kennyd: ${kennyd} <br>
+                    Tony: ${tony} <br>
+                    Brener: ${brener} <br>
+                    Ademiro: ${ademiro} <br>
+                </p>
+                <p><button onclick="verPontos()" id="verPontosPlanilha">Somar Pontos</button></p>
+
+    `
+}
