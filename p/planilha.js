af0bcb768a7b7d3997eea8cda2af4693bb177977
Ruanh <79330021+joseruanh@users.noreply.github.com>
2022-11-14 11:56:33 -0300

    adicionando rolagem de página

--- p/planilha.js
+++ p/planilha.js
@@ -24,3 +24,13 @@ function verPontos(){
                 </p>
     `
 }
+
+function subir()
+{
+    window.scrollTo(0, 0);
+}
+
+function descer()
+{
+    window.scrollTo(0, document.body.scrollHeight);
+}
