e132376e02d12b524d3827e8ad0634b467d30573
Ruanh <79330021+joseruanh@users.noreply.github.com>
2023-01-23 16:05:02 -0300

    resultados primeira semana

--- p/planilha.js
+++ p/planilha.js
@@ -1,11 +1,11 @@
     function verPontos(){
     let ruanh = 4;
-    let jean = 4;
-    let pickles = 3;
+    let jean = 5;
+    let pickles = 4;
     let relampago = 5;
     let kennyd = 4;
     let tony = 5;
-    let brener = 3;
+    let brener = 4;
     let ademiro = 5;
 
     let pontos = window.document.getElementById('pontos');
@@ -14,10 +14,10 @@
     <p style="font-weight: bolder;">Total de pontos <br></p>
                 <p> 
                     Marcos: ${relampago} <br>
+                    Jean: ${jean} <br>
                     Ademiro: ${ademiro} <br>
                     Tony: ${tony} <br>
                     Ruanh: ${ruanh} <br>
-                    Jean: ${jean} <br>
                     Kennyd: ${kennyd} <br>
                     Pickles: ${pickles} <br>
                     Brener: ${brener} <br>
