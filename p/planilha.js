31ce72c70546787946f129c27dbf01f2fc9bf82e
Ruanh <79330021+joseruanh@users.noreply.github.com>
2023-02-27 11:44:10 -0300

    rodadas 11 e 12

--- p/planilha.js
+++ p/planilha.js
@@ -1,12 +1,12 @@
     function verPontos(){
-    let ruanh = 26;
-    let jean = 25;
-    let pickles = 23;
-    let relampago = 20;
+    let ruanh = 33;
+    let jean = 31;
+    let pickles = 30;
+    let relampago = 26;
     let kennyd = 5;
-    let tony = 17;
-    let brener = 23;
-    let ademiro = 24;
+    let tony = 23;
+    let brener = 27;
+    let ademiro = 28;
 
     let pontos = window.document.getElementById('pontos');
     pontos.innerHTML = `
@@ -14,14 +14,14 @@
     <p style="font-weight: bolder;">Total de pontos <br></p>
                 <p>             
                     Ruanh: ${ruanh} <br> 
-                    Jean: ${jean} <br>                   
+                    Jean: ${jean} <br>
+                    Pickles: ${pickles} <br>                   
                     Ademiro: ${ademiro} <br> 
-                    Pickles: ${pickles} <br>
                     Brener: ${brener} <br>  
                     Marcos: ${relampago} <br>
                     Tony: ${tony} <br>                    
-                    Victor: 11 <br>
-                    Pedro: 8 <br>
+                    Victor: 14 <br>
+                    Pedro: 14 <br>
                     Kennyd: ${kennyd} <br>
 
                     <br><br>
